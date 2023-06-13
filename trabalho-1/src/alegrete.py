0cf35162d1e6a39121bcf07eb6af24f9c554c77e
pKlein <pedroklein1@hotmail.com>
2023-06-13 18:58:42 +0000

    config: set trabalho-1 workspace

---
+++ trabalho-1/src/alegrete.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+def compute_mse(b, w, data):
+    """
+    Calcula o erro quadratico medio
+    :param b: float - bias (intercepto da reta)
+    :param w: float - peso (inclinacao da reta)
+    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
+    :return: float - o erro quadratico medio
+    """
+    raise NotImplementedError  # substituir pelo seu codigo
+
+
+def step_gradient(b, w, data, alpha):
+    """
+    Executa uma atualização por descida do gradiente  e retorna os valores atualizados de b e w.
+    :param b: float - bias (intercepto da reta)
+    :param w: float - peso (inclinacao da reta)
+    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
+    :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
+    :return: float,float - os novos valores de b e w, respectivamente
+    """
+    raise NotImplementedError  # substituir pelo seu codigo
+
+
+def fit(data, b, w, alpha, num_iterations):
+    """
+    Para cada época/iteração, executa uma atualização por descida de
+    gradiente e registra os valores atualizados de b e w.
+    Ao final, retorna duas listas, uma com os b e outra com os w
+    obtidos ao longo da execução (o último valor das listas deve
+    corresponder à última época/iteração).
+
+    :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
+    :param b: float - bias (intercepto da reta)
+    :param w: float - peso (inclinacao da reta)
+    :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
+    :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
+    :return: list,list - uma lista com os b e outra com os w obtidos ao longo da execução
+    """
+    raise NotImplementedError  # substituir pelo seu codigo
---
+++ trabalho-1/src/test_alegrete.py
@@ -0,0 +1,29 @@
+import unittest
+import alegrete
+import numpy as np
+
+
+class TestAlegrete(unittest.TestCase):
+
+    def test_compute_mse(self):
+        data = np.genfromtxt('alegrete.csv', delimiter=',')
+        mse = alegrete.compute_mse(0, 0, data)
+        self.assertAlmostEqual(66.78348986604624, mse, 8)  # comparacao de floats com 9 casas de precisao
+
+    def test_step_gradient(self):
+        # dataset ficticio
+        data = np.array([
+            [1, 3],
+            [2, 4],
+            [3, 4],
+            [4, 2]
+        ])
+
+        new_b, new_w = alegrete.step_gradient(1, 1, data, alpha=0.1)
+        # comparacao de floats com precisao de 11 casas
+        self.assertAlmostEqual(0.95, new_b, 11)
+        self.assertAlmostEqual(0.55, new_w, 11)
+
+
+if __name__ == '__main__':
+    unittest.main()

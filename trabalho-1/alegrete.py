d455322d0fbfa4e4d4006ec956b114398a059804
pKlein <pedroklein1@hotmail.com>
2023-06-30 18:51:31 +0000

    refactor: alegrete2 now main alegrete with dataset normalization

--- trabalho-1/alegrete.py
+++ trabalho-1/alegrete.py
@@ -1,16 +1,5 @@
 import numpy as np
 
-def compute_hypothesis(b, w, x):
-    """
-    Calcula a altura da reta para um dado valor de x.
-    :param b: float - bias (intercepto da reta)
-    :param w: float - peso (inclinacao da reta)
-    :param x: float - valor de x
-    :return: float - altura da reta para o dado valor de x
-    """
-    return b + w * x
-
-
 def compute_mse(b, w, data):
     """
     Calcula o erro quadratico medio
@@ -19,13 +8,22 @@ def compute_mse(b, w, data):
     :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
     :return: float - o erro quadratico medio
     """
-    N = data.shape[0]
-    squared_error = 0
-    
-    for (area, price) in data:
-        squared_error += pow(compute_hypothesis(b, w, area) - price, 2)
 
-    return squared_error / N
+    numLinhas = (data.shape[0]) #shape[0] pois quero pegar num de linhas
+
+    #x lidos do csv -> é a primeira coluna 
+    #y lidos do csv -> segunda coluna = previsão real 
+    #f(x) = func -> predição 
+    soma_erros_quad = 0
+
+    for area, preco in data:
+        #f(x) = b + w*x
+        func = b + w*area 
+        soma_erros_quad += ((func - preco)**2)
+
+    erro_quad_medio = soma_erros_quad / numLinhas
+
+    return erro_quad_medio
 
 
 def step_gradient(b, w, data, alpha):
@@ -37,19 +35,22 @@ def step_gradient(b, w, data, alpha):
     :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
     :return: float,float - os novos valores de b e w, respectivamente
     """
-    b_gradient = 0
-    w_gradient = 0
-    N = data.shape[0]
+    numLinhas = (data.shape[0]) #shape[0] pois quero pegar num de linhas
+    grad_b = 0
+    grad_w = 0 
 
-    for (area, price) in data:
-        b_gradient += (2 / N) * (compute_hypothesis(b, w, area) - price)
-        w_gradient += (2 / N) * (compute_hypothesis(b, w, area) - price) * area
+    for area, preco in data:
+        #f(x) = b + w*x
+        func = b + w*area 
+        #calculo das derivadas parciais
+        grad_b += (2/numLinhas) * (func - preco)
+        grad_w += (2/numLinhas) * (func - preco) * area
 
-    new_b = b - alpha * b_gradient
-    new_w = w - alpha * w_gradient
+    #Atualiza b e w
+    new_b = b - (alpha*grad_b)
+    new_w = w - (alpha*grad_w)
 
     return new_b, new_w
-
 
 def fit(data, b, w, alpha, num_iterations):
     """
@@ -66,17 +67,16 @@ def fit(data, b, w, alpha, num_iterations):
     :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
     :return: list,list - uma lista com os b e outra com os w obtidos ao longo da execução
     """
-    b_list = []
-    w_list = []
+    lista_b = []
+    lista_w = []
 
-    min_val = np.min(data, axis=0)
-    max_val = np.max(data, axis=0)
-    normalized_dataset = (data - min_val) / (max_val - min_val)
+    min_val = np.min(data, axis=0) # pega a tupla de menores valores do dataset (de area e preco)
+    max_val = np.max(data, axis=0)  # pega a tupla de maiores valores do dataset (de area e preco)
+    normalized_dataset = (data - min_val) / (max_val - min_val) # normaliza os dados
 
-
-    for i in range(num_iterations):
+    for epoch in range(num_iterations):
         b, w = step_gradient(b, w, normalized_dataset, alpha)
-        b_list.append(b)
-        w_list.append(w)
-
-    return b_list, w_list
+        lista_b.append(b)
+        lista_w.append(w)
+    
+    return lista_b, lista_w

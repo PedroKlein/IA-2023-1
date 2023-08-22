69f3aac0a64d74fde37c9f520b02151860730956
Felipe Bertoglio <felipe.bertoglio@gmail.com>
2023-08-21 21:49:13 -0300

    feat-monte-carlo-

--- Trabalho-4/kit_games/advsearch/your_agent/mcts.py
+++ Trabalho-4/kit_games/advsearch/your_agent/mcts.py
@@ -1,76 +1,78 @@
 import random
 from typing import Tuple
-from collections import defaultdict
+from ..othello.gamestate import GameState
+from math import sqrt, log
 
-class Node:
-    def __init__(self, state, parent=None):
+# Voce pode criar funcoes auxiliares neste arquivo
+# e tambem modulos auxiliares neste pacote.
+#
+# Nao esqueca de renomear 'your_agent' com o nome
+# do seu agente.
+
+
+class MCTSNode:
+    def __init__(self, state: GameState, parent=None, last_move=None):
         self.state = state
-        self.parent = parent
-        self.children = []
-        self.wins = 0
+        self.parent:GameState = parent
+        self.children:GameState = []
+        self.last_move: Tuple[int, int] = last_move
         self.visits = 0
+        self.value = 0
 
-    def select(self):
-        best_value = float('-inf')
-        best_nodes = []
+    def is_fully_expanded(self):
+        return len(self.children) == len(self.state.legal_moves())
 
-        for child in self.children:
-            ucb = 0 if child.visits == 0 else child.wins / child.visits + (2 * (self.visits ** 0.5) / child.visits)
-            if ucb == best_value:
-                best_nodes.append(child)
-            if ucb > best_value:
-                best_value = ucb
-                best_nodes = [child]
-
-        return random.choice(best_nodes)
-
-    def expand(self):
-        legal_moves = self.state.legal_moves()
-        for move in legal_moves:
-            if move not in [child.state.board.board for child in self.children]:
-                self.children.append(Node(self.state.next_state(move), self))
-
-    def simulate(self):
-        current_state = self.state.copy()
-        while not current_state.is_terminal():
-            current_state = current_state.next_state(random.choice(list(current_state.legal_moves())))
-        return current_state.winner()
-
-    def backpropagate(self, result):
-        self.visits += 1
-        if result == self.state.player:
-            self.wins += 1
-        if self.parent:
-            self.parent.backpropagate(result)
+    def best_child(self, exploration_weight=1.0):
+        best_child = max(self.children, key=lambda child: child.value / (child.visits + 1e-6)
+                         + exploration_weight * sqrt(log(self.visits + 1) / (child.visits + 1e-6)))
+        return best_child
 
 
-def mcts(state, num_simulations=1000):
-    root = Node(state)
 
-    for _ in range(num_simulations):
+
+def make_move(state:GameState) -> Tuple[int, int]:
+    """
+    Returns a move for the given game state.
+    The game is not specified, but this is MCTS and should handle any game, since
+    their implementation has the same interface.
+
+    :param state: state to make the move
+    :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
+    """
+    
+    root = MCTSNode(state)
+
+    iterations = 1000
+    for _ in range(iterations):
         node = root
-        # Selection
-        while node.children:
-            node = node.select()
 
-        # Expansion
+        # Selection and expansion
+        while not node.state.is_terminal() and node.is_fully_expanded():
+            node = node.best_child()
+
         if not node.state.is_terminal():
-            node.expand()
+            unexpanded_moves = node.state.legal_moves() - {child.last_move for child in node.children}
+            move = random.choice(list(unexpanded_moves))
+            next_state = node.state.next_state(move)
+            node.children.append(MCTSNode(next_state, parent=node, last_move=move))
+            node = node.children[-1]
 
         # Simulation
-        outcome = node.simulate()
+        sim_state = node.state.copy()
+        while not sim_state.is_terminal():
+            random_move = random.choice(list(sim_state.legal_moves()))
+            sim_state = sim_state.next_state(random_move)
 
         # Backpropagation
-        node.backpropagate(outcome)
+        value = 1 if sim_state.winner() == state.player else 0
+        while node is not None:
+            node.visits += 1
+            node.value += value
+            node = node.parent
 
-    # Choose the child with the highest number of visits as the next move
-    return max(root.children, key=lambda c: c.visits).state.board.board
+    best_child = root.best_child(exploration_weight=0.0)
+    best_move = best_child.last_move
 
+    print(f"best move: {best_move}")
 
-def make_move(state) -> Tuple[int, int]:
-    action = mcts(state)
-    for move, st in state.game_tree[state.board.board].items():
-        if st == action:
-            return move
-
-    return (-1, -1)  # Default move if nothing is found.
+    return best_move
